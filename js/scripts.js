74d10230ec6cf1f5ee67ddc2fce2047779dbc815
Lar Harney <lar@Lars-MacBook-Air.local>
2021-01-10 18:41:03 +0000

    Home, Projects and contact sections added;

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 100, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
